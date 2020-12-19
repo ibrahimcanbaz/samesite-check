95b1e91b4df5ba0403505932607fa90791928d09
canbazia <canbazia@gmail.com>
2020-12-20 00:20:32 +0200

    firstcommit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    const cookie = 'user=can; samesite=strict; secure';
+    res.setHeader('set-cookie', [cookie]);
+    res.send('ok')
+  })
+
+  app.get('/check', (req, res) => {
+    const cookie = req.headers.cookie;
+    if (cookie) {
+        res.send('successs');
+    } else {
+        res.sendStatus(403);
+        res.end();
+    }
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
